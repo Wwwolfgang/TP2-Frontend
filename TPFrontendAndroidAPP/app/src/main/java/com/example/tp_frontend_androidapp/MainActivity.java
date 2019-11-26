a87764a33636a43a5f0497e190923e6369c421e7
wwwolfgang <wwwolfgang469@gmail.com>
2019-11-26 18:20:58 -0300

    Android proyect

---
+++ TPFrontendAndroidAPP/app/src/main/java/com/example/tp_frontend_androidapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.tp_frontend_androidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText campoNombreUsuario;
+    EditText campoPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        campoNombreUsuario=findViewById(R.id.txtNombreUsuario);
+        campoPassword=findViewById(R.id.txtPassword);
+    }
+
+    public void ingresar(View view) {
+        if (campoNombreUsuario.getText().toString().equals("admin")
+                && campoPassword.getText().toString().equals("123")) {
+
+            Intent intentNewActivity = new Intent(MainActivity.this, ReservaActivity.class);
+            Bundle b = new Bundle();
+            b.putString("usuario",campoNombreUsuario.getText().toString());
+            intentNewActivity.putExtras(b);
+            startActivity(intentNewActivity);
+
+
+        }
+
+    }
+}
---
+++ TPFrontendAndroidAPP/app/src/main/java/com/example/tp_frontend_androidapp/ReservaActivity.java
@@ -0,0 +1,4 @@
+package com.example.tp_frontend_androidapp;
+
+public class ReservaActivity {
+}
